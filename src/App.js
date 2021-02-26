f7ea44d65290a0016fe0254bf82719e33363a001
kcmikee <michael.esenwa@gmail.com>
2021-02-26 20:59:22 +0100

    log in integrated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {BrowserRouter, Route,Link} from 'react-router-dom'
+import { signout } from './actions/userActions';
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from './screens/Homescreen'
 import ProductScreen from './screens/Productscreen'
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
     const cart = useSelector((state) => state.cart);
     const { cartItems } = cart;
+
+    const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -24,13 +33,29 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}    
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
             <Route path='/' component={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen}/>
             <Route path='/cart/:id?' component={CartScreen}/>
+            <Route path="/signin" component={SigninScreen}/>
         </main>
       </div>
     </BrowserRouter>
---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../screens/LoadingBox';
+import MessageBox from '../screens/MessageBox';
+
+export default function SigninScreen(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const redirect = props.location.search
+    ? props.location.search.split('=')[1]
+    : '/';
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+      e.preventDefault();
+      // TODO: sign in action
+      dispatch(signin(email, password));
+    };
+
+    useEffect(() => {
+        if (userInfo) {
+          props.history.push(redirect);
+        }
+      }, [props.history, redirect, userInfo]);
+
+  return (
+    <div>
+      <form onSubmit={submitHandler} className="form">
+        <div>
+          <h1>Sign In</h1>
+        </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter Email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+            <label htmlFor="">Password</label>
+            <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            New customer? <Link to="/register">Create your account</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
